64820a9df096fa85bc25ccfae06ad6f7e8a7b5ec
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2018-10-11 11:59:01 +0200

    Implement predict function.

--- ann/src/main.c
+++ ann/src/main.c
@@ -3,55 +3,87 @@
 #include <string.h>
 #include <assert.h>
 #include "neuron.h"
-
+#include "neural_network.h"
 
 #define MAX_DIMENSIONS 784
 
-int main() {
+void printData(int label, float data[MAX_DIMENSIONS]) {
+  unsigned int i;
+
+  printf("Label: %d  ", label);
+  printf(" Data: [");
+  for (i = 0; i < MAX_DIMENSIONS; i++) {
+    if (i > 0)
+      printf(", ");
+    printf("%f", data[i]);
+  }
+  printf("]");
+}
+
+int predict(float data[MAX_DIMENSIONS], unsigned int label) {
+  #include "models/nn_3_layers_32_neurons.h"
+
+  FILE * fp;
+
+  fp = fopen("result.csv", "w");
+  if (fp == NULL) {
+    printf("Opening file 'result.csv' failed!\n");
+    return -1;
+  }
+
+  Input input;
+  Input_ctor(&input, data, MAX_DIMENSIONS);
+  float *result = NeuralNetwork_compute(&nn, &input);
+
+  // outputSize
+  unsigned int i, predictedLabel = 0;
+  float maxValue = 0.0;
+  for (i = 0; i < outputSize; i++) {
+    if (result[i] > maxValue) {
+      maxValue = result[i];
+      predictedLabel = i;
+    }
+  }
+
+  printf("True label: %d  Predicted label: %d \n", label, predictedLabel);
+
+  return 0;
+}
+
+int parseTestData() {
   FILE * fp;
   char * line = NULL;
   size_t len = 0;
   ssize_t read;
 
-  printf("Hello, World!\n");
-
   fp = fopen("test_data.csv", "r");
   if (fp == NULL) {
     printf("File open failed!\n");
-
     return -1;
   }
 
-
-  int label = -1;
+  int result;
   float data[MAX_DIMENSIONS];
+  unsigned int i, label;
+  char *field;
 
   while ((read = getline(&line, &len, fp)) != -1) {
-    //printf("Retrieved line of length %zu :\n", read);
-    //printf("%s", line);
-
-    char *field;
+    // Parse the label field
     field = strtok(line, ",");
     assert(field != NULL);
-
     label = atoi(field);
-    unsigned int i;
+
+    // Parse the other fields
     for (i = 0; i < MAX_DIMENSIONS; i++) {
       // Read next field value
-      field = strtok (NULL, ",");
+      field = strtok(NULL, ",");
       assert(field != NULL);
       data[i] = atof(field);
     }
 
-    printf("Label: %d  ", label);
-    printf(" Data: [");
-
-    for (i = 0; i < MAX_DIMENSIONS; i++) {
-      if (i > 0)
-        printf(", ");
-      printf("%f", data[i]);
-    }
-    printf("]");
+    //printData(label, data);
+    result = predict(data, label);
+    assert(result == 0);
   }
 
   fclose(fp);
@@ -59,4 +91,9 @@ int main() {
     free(line);
 
   return 0;
+}
+
+int main() {
+  printf("Hello, World!\n");
+  return parseTestData();
 }
