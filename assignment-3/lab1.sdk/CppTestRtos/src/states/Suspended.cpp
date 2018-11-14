debc61975f54347cc93c879efa731b9c81be1158
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2018-11-14 13:48:45 +0100

    Add Suspended state.

---
+++ assignment-3/lab1.sdk/CppTestRtos/src/states/Suspended.cpp
@@ -0,0 +1,13 @@
+#include "Suspended.h"
+#include "Ready.h"
+//#include "RealTimeLoop.h"
+
+Suspended::Suspended() : State("Suspended") { }
+
+void Suspended::Stop(Context* ctx) {
+	ctx->Transition(Ready::GetInstance());
+}
+
+void Suspended::Resume(Context* ctx) {
+	//ctx->Transition(RealTimeLoop::GetInstance());
+}
