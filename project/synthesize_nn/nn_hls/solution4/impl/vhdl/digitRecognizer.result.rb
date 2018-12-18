9fe338ed97d13aed17de7f3558d35a8bd4138b10
Lukas <lxhdgd@gmail.com>
2018-12-18 21:03:31 +0100

    Added solution for pipelined neurons (solution4)

---
+++ project/synthesize_nn/nn_hls/solution4/impl/vhdl/digitRecognizer.result.rb
@@ -0,0 +1,14 @@
+$Footmark = "FPGA_Xilinx"
+$Description = "by Vivado"
+
+
+#=== Resource usage ===
+$SLICE = "0"
+$LUT = "848"
+$FF = "887"
+$DSP = "5"
+$BRAM ="70"
+$SRL ="2"
+#=== Final timing ===
+$TargetCP = "8.000"
+$CP = "6.116"
